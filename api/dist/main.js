7fcf9a0387ee00d1d5d16164c5b0699845d7910f
bencso <bencsoofficial@gmail.com>
2025-09-20 21:57:50 +0200

    Add Swagger API documentation setup
    Integrated @nestjs/swagger to provide API documentation for the CashTracker API. Added SwaggerModule configuration and setup in main.ts, updated dependencies in package.json and yarn.lock.

--- api/dist/main.js
+++ api/dist/main.js
@@ -2,8 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, { abortOnError: false });
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('CashTracker API')
+        .setDescription('Ez az első nestJS projektem, ezzel a projektel gyakorlom és ismerem meg a nestJS-t, a projekt során a [nestJS hivatalos dokumentációját](https://docs.nestjs.com/) fogom végigkövetni, és próbálom a legkevesebb AI-t segítséget használni.')
+        .setVersion('0.1')
+        .build();
+    const documentFactory = () => swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('api', app, documentFactory);
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
+
+  //Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('CashTracker API')
+    .setDescription(
+      'Ez az első nestJS projektem, ezzel a projektel gyakorlom és ismerem meg a nestJS-t, a projekt során a [nestJS hivatalos dokumentációját](https://docs.nestjs.com/) fogom végigkövetni, és próbálom a legkevesebb AI-t segítséget használni.',
+    )
+    .setVersion('0.1')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
